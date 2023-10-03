3d8ab239a6e979dbc33ce464af4b447daddf8b48
Alexandra <ale.bonna@hotmail.com>
2023-10-03 17:28:02 -0300

    resolução do desafio dio

--- Models/Iphone.cs
+++ Models/Iphone.cs
@@ -1,8 +1,15 @@
 namespace DesafioPOO.Models
 {
-    // TODO: Herdar da classe "Smartphone"
-    public class Iphone
+    public class Iphone : Smartphone
     {
-        // TODO: Sobrescrever o método "InstalarAplicativo"
+        public  Iphone(string numero, string imie, string modelo, int memoria) : base(numero, imie, modelo, memoria)
+        {
+
+        }
+
+        public override void  InstalarAplicativo(string nomeApp)
+        { 
+            Console.WriteLine($"Instalando o aplicativo {nomeApp} no IPhone");
+        } 
     }
 }
--- Models/Nokia.cs
+++ Models/Nokia.cs
@@ -1,8 +1,16 @@
 namespace DesafioPOO.Models
 {
     // TODO: Herdar da classe "Smartphone"
-    public class Nokia
+    public class Nokia : Smartphone
     {
-        // TODO: Sobrescrever o método "InstalarAplicativo"
+        public Nokia(string numero, string imie, string modelo, int memoria) : base(numero, imie, modelo, memoria)
+        {
+
+        }
+
+        public override void InstalarAplicativo (string nomeApp) 
+        {
+            Console.WriteLine($"Instalando aplicativos {nomeApp} no Nokia");
+        }
     }
 }
